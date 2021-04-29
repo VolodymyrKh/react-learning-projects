a4b347cb45598ec8815d93ca68da21158ea9acf8
Volodymyr <volodymyr_kh@hotmail.com>
2021-04-29 09:56:50 +0300

    price fixing

--- 01_componets_list/src/components/Tour.js
+++ 01_componets_list/src/components/Tour.js
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 
 const Tour = ({ id, name, info, image, price, removeItem }) => {
   const [readMore, setReadMore] = useState(false);
+  
+  const roundStr = str => {
+      return Math.trunc((Number.parseFloat(str.split(',').join('.'))*100))/100
+  }
 
+  console.log(roundStr(price));
+  
   return (
     <li>
       <article className="single-tour">
         <img src={image} alt="tour img" />
         <div className="tour-info">
           <h4>{name}</h4>
-          <h4 className="tour-price">${price}</h4>
+          <h4 className="tour-price">${roundStr(price)}</h4>
         </div>
         <p>
           {readMore ? info : `${info.substring(0, 175)}...`}
