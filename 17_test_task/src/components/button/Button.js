0013bccbc77c3abb6f5007601a17ebdb51a9dbd2
Volodymyr <volodymyr_kh@hotmail.com>
2021-06-09 03:12:11 +0300

    input styles added

--- 17_test_task/src/components/button/Button.js
+++ 17_test_task/src/components/button/Button.js
@@ -1,13 +1,21 @@
 import React from "react";
 import styles from "./button.module.scss";
 
-export const Button = ({ text, src }) => {
+export const Button = ({ text, src, mb }) => {
   return (
-    <a className={styles.btn} href="#">
-      <div className={styles.btn__icon}>
-        <img src={src} alt="button icon" />
-      </div>
-      <p className={styles.btn__text}>{text}</p>
-    </a>
+    <div className={styles.btn} style={{marginBottom: mb}}>
+      <a className={styles.btn__link} href="#">
+        <div className={styles.btn__icon}>
+          <img src={src} alt="button icon" />
+        </div>
+        <p className={styles.btn__text}>{text}</p>
+      </a>
+    </div>
   );
+};
+
+Button.defaultProps = {
+  text: "Default button",
+  src: "./icons/google.svg",
+  mb: '0',
 };
--- 17_test_task/src/components/form/SignIn.js
+++ 17_test_task/src/components/form/SignIn.js
@@ -14,12 +14,46 @@ export const SignIn = () => {
           </a>
         </p>
         <Button
-          style={{ marginBottom: "1rem" }}
-          {...{ text: "Continue with Google", src: "./icons/google.svg" }}
+          {...{
+            text: "Continue with Google",
+            src: "./icons/google.svg",
+            mb: "1rem",
+          }}
         />
         <Button
-          {...{ text: "Continue with Micrsoft", src: "./icons/microsoft.svg" }}
+          {...{
+            text: "Continue with Micrsoft",
+            src: "./icons/microsoft.svg",
+            mb: "0.75rem",
+          }}
         />
+        <span>or</span>
+
+        <div className={styles.logIn__email}>
+          <label htmlFor="email" className={styles.logIn__label}>
+            user email
+          </label>
+          <input
+            className={styles.logIn__input}
+            id="email"
+            type="email"
+            placeholder="email"
+            required
+          />
+        </div>
+
+        <div className={styles.logIn__password}>
+          <label htmlFor="password" className={styles.logIn__label}>
+            user password
+          </label>
+          <input
+            className={styles.logIn__input}
+            id="password"
+            type="password"
+            placeholder="password"
+            required
+          />
+        </div>
       </form>
     </div>
   );
